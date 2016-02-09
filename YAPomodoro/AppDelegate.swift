e0fcca4469731205c10291dea320130af5ed1309
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2016-02-09 09:40:33 +0900

    ウインドウの位置を保存したり、すべてのウインドウを閉じたらアプリを終了するようにしたり

--- YAPomodoro/AppDelegate.swift
+++ YAPomodoro/AppDelegate.swift
@@ -11,16 +11,19 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        // ウインドウの位置やサイズを保存する
+        let window = NSApp.windows.first
+        window?.setFrameAutosaveName("mainWindow")
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
-
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        // ウインドウをすべて閉じたらアプリケーションを終了
+        return true
+    }
 }
 
